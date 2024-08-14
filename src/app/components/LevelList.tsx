bbe41beb37a8efaadcc535775e047da230bd5783
timothymarriott <timothyrmarriott@gmail.com>
2024-08-14 17:52:39 +0800

    Removed download alert

--- src/app/components/LevelList.tsx
+++ src/app/components/LevelList.tsx
@@ -74,9 +74,8 @@ export function LevelListView(props: {list: LevelList}) {
                             }}> <b>Delete</b> </DropdownMenuItem>
 
                             <DropdownMenuItem onClick={(e) => {
-                                const url = document.URL + "/api/download?id=" + level.guid;
+                                const url = document.URL + "api/download?id=" + level.guid;
 
-                                alert(url);
                                 
                                 const obj = document.createElement("a");
                                 obj.href = url;
